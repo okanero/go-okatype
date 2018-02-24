4199526673282890defe6c96ea797814ce9e6899
Charles Iliya Krempeaux <iamreiver@gmail.com>
2018-02-24 03:24:16 -0800

    updated docs

--- doc.go
+++ doc.go
@@ -1,4 +1,13 @@
 /*
 Package okatype (i.e., Okanero type) provides basic types used for implementing the Okanero protocol.
+
+With the Okanero protocol, there is a message-oriented layer to it.
+
+The Okanero protocol calls its messages "datum".
+
+(In Golang, we can think of serialized Okanero datum as a []byte.)
+
+(You could probably safely conflate an Okanero datum with a "block" in a "blockchain".)
+
 */
 package okatype
