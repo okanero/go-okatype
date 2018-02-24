eefc09d550ba0b13133296c4c1cfe7707592a15a
Charles Iliya Krempeaux <iamreiver@gmail.com>
2018-02-23 22:08:58 -0800

    okatype_magic.Reader

---
+++ magic/reader.go
@@ -0,0 +1,22 @@
+package okatype_magic
+
+import (
+	"io"
+)
+
+// Reader is an interface that wraps a full suite of reading functionality.
+//
+// The methods in this interface are meant to match the methods in the bytes.Reader struct.
+type Reader interface {
+	Len() int
+	Read(b []byte) (n int, err error)
+	ReadAt(b []byte, off int64) (n int, err error)
+	ReadByte() (byte, error)
+	ReadRune() (ch rune, size int, err error)
+	Reset(b []byte)
+	Seek(offset int64, whence int) (int64, error)
+	Size() int64
+	UnreadByte() error
+	UnreadRune() error
+	WriteTo(w io.Writer) (n int64, err error)
+}
