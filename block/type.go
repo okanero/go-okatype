0664cebecd9a6bdbd116b708d57f48f2b95e5cfd
Charles Iliya Krempeaux <iamreiver@gmail.com>
2018-02-26 13:05:32 -0800

    okatype_block.Type

--- block/type.go
+++ block/type.go
@@ -2,14 +2,18 @@ package okatype_block
 
 import (
 	"github.com/okanero/go-okatype/datum"
+	"github.com/okanero/go-okatype/endorser"
 	"github.com/okanero/go-okatype/nonce"
+	"github.com/okanero/go-okatype/signature"
 	"github.com/okanero/go-okatype/trace"
 )
 
 type Type struct {
-	Datum  okatype_datum.Type
-	
-	Trace  okatype_trace.Type
-	Time   okatype_time.Type
-	Nonce  okatype_nonce.Type
+	Datum okatype_datum.Type
+
+	Trace             okatype_trace.Type
+	Time              okatype_time.Type
+	Ensorser          okatype_endorser.Type
+	EndorserSignature okatype_signature.Type
+	Nonce             okatype_nonce.Type
 }
