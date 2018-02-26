e022e265fd6c2f35571ec0ba7b1d496b9561a5c3
Charles Iliya Krempeaux <iamreiver@gmail.com>
2018-02-26 13:19:31 -0800

    okatype_datum.Type

--- datum/datum.go
+++ datum/datum.go
@@ -1,15 +1,33 @@
 package okatype_datum
 
 import (
+	"github.com/okanero/go-okatype/agent"
+	"github.com/okanero/go-okatype/count"
+	"github.com/okanero/go-okatype/hash"
+	"github.com/okanero/go-okatype/interaction"
 	"github.com/okanero/go-okatype/length"
 	"github.com/okanero/go-okatype/magic"
+	"github.com/okanero/go-okatype/nonce"
+	"github.com/okanero/go-okatype/payload"
+	"github.com/okanero/go-okatype/signature"
+	"github.com/okanero/go-okatype/time"
 	"github.com/okanero/go-okatype/version"
 )
 
 type Type struct {
-	Magic        okatype_magic.Type       //  8 bytes \
-	Version      okatype_version.Type     //  8 bytes |
-	Length       okatype_length.Type      //  8 bytes |_ cache line
-	                                      //  8 bytes |
-	                                      // 32 bytes /
+	Genesis okatype_hash.Type
+	Prev    okatype_hash.Type
+	Count   okatype_count.Type
+
+	Lateral1 okatype_hash.Type
+	Lateral2 okatype_hash.Type
+	Lateral3 okatype_hash.Type
+
+	Payload okatype_payload.Type
+
+	Interaction     okatype_interaction.Type
+	Time            okatype_time.Type
+	Author          okatype_agent.Type
+	AuthorSignature okatype_signature.Type
+	Nonce           okatype_nonce.Type
 }
